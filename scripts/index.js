18c53474809e1a9d884dee419eeafe09f2c73dfe
Richard <nnejirichard@yahoo.com>
2025-06-04 18:38:31 +0100

    added js to index

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ getDate()
 
 let detailsOne = document.querySelector('.js-details-one')
 let detailsTwo = document.querySelector('.js-details-two')
+let detailsThree = document.querySelector('.js-details-three')
+
 window.addEventListener("scroll", () =>{
 if (window.pageYOffset > 390){
     detailsOne.style = "opacity:1"
@@ -11,12 +13,20 @@ if (window.pageYOffset > 390){
 if (window.pageYOffset < 390){
     detailsOne.style = "opacity:0"
 }
-if(window.pageYOffset < 630){
+if(window.pageYOffset < 1150){
 detailsTwo.style = "opacity:0"
 }
-if(window.pageYOffset > 646){
+if(window.pageYOffset > 1150){
 detailsTwo.style = "opacity:1"
 }
-
+if (window.pageYOffset > 1710){
+    detailsThree.style = "opacity:1"
+    detailsThree.style = "transform: translateX(0px)"
+   }
+   if (window.pageYOffset < 1710){
+    detailsThree.style = "opacity:0"
+}
 })
+
+
 window.removeEventListener('scroll')
