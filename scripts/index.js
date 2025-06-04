279f3533d196a587eaa9e663666bcd89161adaa4
Richard <nnejirichard@yahoo.com>
2025-06-04 22:05:22 +0100

    final js added to index

--- scripts/index.js
+++ scripts/index.js
@@ -10,20 +10,21 @@ if (window.pageYOffset > 390){
     detailsOne.style = "opacity:1"
     detailsOne.style = "transform: translateX(0px)"
    }
-if (window.pageYOffset < 390){
+else if (window.pageYOffset < 390){
     detailsOne.style = "opacity:0"
 }
 if(window.pageYOffset < 1150){
 detailsTwo.style = "opacity:0"
 }
-if(window.pageYOffset > 1150){
+else if(window.pageYOffset > 1150){
 detailsTwo.style = "opacity:1"
+detailsTwo.style = "transform: translateX(50px)"
 }
 if (window.pageYOffset > 1710){
     detailsThree.style = "opacity:1"
     detailsThree.style = "transform: translateX(0px)"
    }
-   if (window.pageYOffset < 1710){
+else if (window.pageYOffset < 1710){
     detailsThree.style = "opacity:0"
 }
 })
