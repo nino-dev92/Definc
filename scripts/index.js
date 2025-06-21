46b04681b092aa896c019189e9c0eb9fa6bfe13c
ninodev92 <nnejirichard@yahoo.com>
2025-06-21 08:18:40 +0100

    index JS done

--- scripts/index.js
+++ scripts/index.js
@@ -10,21 +10,21 @@ if (window.scrollY > 390){
     detailsOne.style = "opacity:1"
     detailsOne.style = "transform: translateX(0px)"
    }
-else if (window.pageYOffset < 390){
+else if (window.scrollY < 390){
     detailsOne.style = "opacity:0"
 }
-if(window.pageYOffset < 1150){
+if(window.scrollY < 1150){
 detailsTwo.style = "opacity:0"
 }
-else if(window.pageYOffset > 1150){
+else if(window.scrollY > 500){
 detailsTwo.style = "opacity:1"
 detailsTwo.style = "transform: translateX(50px)"
 }
-if (window.pageYOffset > 1710){
+if (window.scrollY > 1650){
     detailsThree.style = "opacity:1"
     detailsThree.style = "transform: translateX(0px)"
    }
-else if (window.pageYOffset < 1710){
+else if (window.scrollY < 1710){
     detailsThree.style = "opacity:0"
 }
 })
